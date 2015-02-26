86df30c4890ddeac92f35a7cdf86b8d41b34b17c
niharikayadav <niharika.yadav7@gmail.com>
2015-02-26 01:13:08 -0800

    complete synchronized

--- ClientHandler.java
+++ ClientHandler.java
@@ -3,14 +3,14 @@ package MiddleManServer;
 import java.io.*;
 import java.net.*;
 import java.util.*;
-import java.util.Map.Entry;
 
 public class ClientHandler extends Thread {
 
 	private Socket cclient;
 	private HashMap<URL, byte[]> urlHashMap;
 	private LinkedList<URL> urlList;
-	public ClientHandler(Socket client, HashMap<URL, byte[]> urlHashMap, LinkedList<URL> urlList) {
+
+	public ClientHandler(Socket client, HashMap<URL, byte[]> urlHashMap,LinkedList<URL> urlList) {
 		this.cclient = client;
 		this.urlHashMap = urlHashMap;
 		this.urlList = urlList;
@@ -23,77 +23,66 @@ public class ClientHandler extends Thread {
 		BufferedOutputStream realServerOutgoing = null;
 		BufferedInputStream realServerIncoming = null;
 		Socket realServerSocket = null;
+		URL clientURL = null;
+		boolean urlFound = false;
 		byte[] cachedContent = new byte[100000];
 
 		try {
-
 			clientOutgoing = new BufferedOutputStream(cclient.getOutputStream());
 			clientIncoming = new BufferedInputStream(cclient.getInputStream());
 
 			// Request which we get from client
 			String clientRequest = "";
-
-			// Take the incoming request
-			byte[] buf = new byte[4096];// create a buffer of bytes to get images(changed from char to bny array )
+			byte[] buf = new byte[4096];// create a buffer of bytes 
+			
 			clientIncoming.read(buf, 0, 4096);
 			clientRequest = new String(buf);
 			String[] stringsInReq = clientRequest.split(" ");
 			String urlString = stringsInReq[1].toString();
-
-			URL clientURL = new URL(urlString);
+			clientURL = new URL(urlString);
 			String hostName = clientURL.getHost();
-			
-			//System.out.println("Hostname is" + hostName);
-			if(urlHashMap.containsKey(clientURL)) {
-				
-				System.out.println("*******In the loop if url is in HashMap!!*****");
-				int index = urlList.indexOf(clientURL); // Find the index of the cached URL
-				System.out.println("The index at which URL is stored in map is:" + index);
-				urlList.remove(index); // Remove the URL from LinkdList from existing position
-				urlList.add(0, clientURL); // Add the URL to the first position in the Linked List
-				clientOutgoing.write(urlHashMap.get(clientURL), 0, urlHashMap.get(clientURL).length); // Fetch the content of the URL from the cached records
-				clientOutgoing.flush();
-			}
-			else {
-				// This is the actual request which will be sent to web server
-				String webserverRequest = stringsInReq[0] + " " + stringsInReq[1] + "\n";// back slash n is shows the end of request
 
+			synchronized (urlHashMap) {
+				if (urlHashMap.containsKey(clientURL)) {
+					urlFound = true;
+					int index = urlList.indexOf(clientURL); // Find the index of the cached URL
+					urlList.remove(index); // Remove the URL from LinkdList from existing position
+					urlList.add(0, clientURL); // Add the URL to the first position in the Linked List
+					clientOutgoing.write(urlHashMap.get(clientURL), 0,urlHashMap.get(clientURL).length); 
+					clientOutgoing.flush();
+				}
+			}
+			
+			if (!urlFound) {
+				// This is the actual request which will be sent to web server
+				String webserverRequest = stringsInReq[0] + " "+ stringsInReq[1] + "\n";// end of request
+		
 				// Create a new socket for connecting to destination server
 				realServerSocket = new Socket(hostName, 80);
-				realServerOutgoing = new BufferedOutputStream(realServerSocket.getOutputStream());
-				realServerIncoming = new BufferedInputStream(realServerSocket.getInputStream());
-			
-				//PrintWriter pIn = new PrintWriter(realServerSocket.getOutputStream());// when request is send as String
+				realServerOutgoing = new BufferedOutputStream(
+						realServerSocket.getOutputStream());
+				realServerIncoming = new BufferedInputStream(
+						realServerSocket.getInputStream());
 
 				System.out.println(webserverRequest);
 				realServerOutgoing.write(webserverRequest.getBytes());
 				realServerOutgoing.flush();
-				//pIn.print(webserverRequest);
-				//pIn.flush();// shows the end of buffered request
-			
-				/*int bufCount = 0;
+
+				int bufCount = 0;
+				int initial = 0;
 				while ((bufCount = realServerIncoming.read(buf, 0, 4096)) > 0) {
-					System.out.println("The number of bytes read are: "+ bufCount );
 					clientOutgoing.write(buf, 0, bufCount);
 					clientOutgoing.flush();
-				}*/
-				int bufCount = 0;
-				int initial =0;
-				while ((bufCount = realServerIncoming.read(buf, 0, 4096)) > 0) {
-						//System.out.println("The number of bytes read are: "+ bufCount );
-						clientOutgoing.write(buf, 0, bufCount);
-						clientOutgoing.flush();
-						if(initial <= cachedContent.length) {
-							System.out.println("The value of initial is: "+ initial);
-							System.arraycopy(buf, 0, cachedContent, initial, bufCount);
-							initial += bufCount;
-						}
-						
+					
+					if ((initial + bufCount) <= cachedContent.length) {
+						System.arraycopy(buf, 0, cachedContent, initial, bufCount);
+						initial += bufCount;
 					}
-				
-					if(urlList.size() != 100) {
+				}
+
+				synchronized (urlHashMap) {
+					if (urlList.size() != 100) {
 						urlList.add(clientURL);
-						//System.out.println("The value to be stored in HashMap are:" +cachedContent.toString());
 						urlHashMap.put(clientURL, cachedContent);
 					} else {
 						URL getLastInList = urlList.getLast();
@@ -101,32 +90,19 @@ public class ClientHandler extends Thread {
 						urlList.addFirst(clientURL);
 						urlHashMap.remove(getLastInList);
 					}
-					
-					//Printing out the cached result..
-					System.out.println("Data Transfer done for " + webserverRequest);	
-					
-					System.out.println("Printing all the values stored in hashmap: ");
-					Set urlSet = urlHashMap.entrySet();
-					Iterator it = urlSet.iterator();
-					
-					while(it.hasNext()) {
-						@SuppressWarnings("unchecked")
-						Map.Entry<URL, byte[]> mapEntry = (Entry<URL, byte[]>) it.next();
-						System.out.println("The key stored in the HashMap are:  "+ mapEntry.getKey());
-						
-						/*for( int i =0; i<mapEntry.getValue().length; i++) {
-							System.out.println( mapEntry.getValue()[i]);
-						}*/
-					}
+				}
+
+				System.out.println("Data Transfer done for " + webserverRequest);
 			}
+
 		} catch (SocketException e) {
-			e.printStackTrace();
+			System.out.println("The connection is closed for: " + clientURL);
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		} finally {
 
 			try {
-				if(realServerSocket != null) {
+				if (realServerSocket != null) {
 					realServerSocket.close();
 				}
 				cclient.close();
@@ -134,7 +110,5 @@ public class ClientHandler extends Thread {
 				e.printStackTrace();
 			}
 		}
-
 	}
-
 }
